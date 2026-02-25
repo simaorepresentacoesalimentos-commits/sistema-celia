5ffd6278e3a1e8ffc5c5888867033328e89513c8
simaorepresentacoesalimentos-commits <simaorepresentacoesalimentos@gmail.com>
2026-02-25 10:17:22 -0300

    Add files via upload

--- constants.tsx
+++ constants.tsx
@@ -1,6 +1,1 @@
-
-import { Product, Customer, Sale } from './types';
-
-export const MOCK_PRODUCTS: Product[] = [];
-export const MOCK_CUSTOMERS: Customer[] = [];
-export const MOCK_SALES: Sale[] = [];
+Permission denied
